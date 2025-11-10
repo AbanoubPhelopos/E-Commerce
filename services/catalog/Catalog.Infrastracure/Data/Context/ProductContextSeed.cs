84b39e05ce1e20542e217bff14874152c571bd41
abanoub Saweris <abanoubphelopos12@gmail.com>
2025-11-10 15:30:37 +0200

    seeding catalog data

---
+++ services/catalog/Catalog.Infrastracure/Data/Context/ProductContextSeed.cs
@@ -0,0 +1,29 @@
+using System.Text.Json;
+using Catalog.Core.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.Infrastracure.Data
+{
+    public static class ProductContextSeed
+    {
+        public static async Task SeedDataAsync(IMongoCollection<Product> productcollection)
+        {
+            var existingProducts = await productcollection.Find(_ => true).AnyAsync();
+            if (existingProducts)
+                return;
+            var filePath = Path.Combine("Data", "SeedData", "products.json");
+
+            if (!File.Exists(filePath))
+            {
+                Console.WriteLine($"Seed file not found: {filePath}");
+                return;
+            }
+            var productData = await File.ReadAllTextAsync(filePath);
+            var products = JsonSerializer.Deserialize<List<Product>>(productData);
+            if (products?.Any() is true)
+            {
+                await productcollection.InsertManyAsync(products);
+            }
+        }
+    }
+}
---
+++ services/catalog/Catalog.Infrastracure/Data/Context/TypeContextSeed.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.Infrastracure.Data.Context
+{
+    public class TypeContextSeed
+    {
+        
+    }
+}
