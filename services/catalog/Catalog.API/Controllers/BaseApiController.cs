a95b518372ead3fd8d78cdc5fcc3fd79a82c7ba8
abanoub Saweris <abanoubphelopos12@gmail.com>
2025-11-13 17:11:07 +0200

    solve seeding bug

--- services/catalog/Catalog.API/Controllers/BaseApiController.cs
+++ services/catalog/Catalog.API/Controllers/BaseApiController.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace Catalog.API.Controllers
 {
     [ApiVersion("1")]
-    [Route("api/v(version:apiVersion)/[controller]")]
+    [Route("api/v{version:apiVersion}/[controller]")]
     [ApiController]
     public class BaseApiController : ControllerBase
     {
