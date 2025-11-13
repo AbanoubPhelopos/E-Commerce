d00067cc84dd515e35541d3906044a4579276a5f
abanoub Saweris <abanoubphelopos12@gmail.com>
2025-11-13 12:21:26 +0200

    Setting up program.cs

--- services/catalog/Catalog.API/Program.cs
+++ services/catalog/Catalog.API/Program.cs
@@ -1,9 +1,28 @@
+using Catalog.Application.Mappers;
+
 var builder = WebApplication.CreateBuilder(args);
 
 
 builder.Services.AddControllers();
+
+builder.Services.AddAutoMapper(typeof(ProductMappingProfile).Assembly);
+
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Title = "Catalog API",
+        Version = "v1",
+        Description = "this api for Catalog microservice in an e-commerce application",
+        Contact = new Microsoft.OpenApi.Models.OpenApiContact
+        {
+            Name = "Abanoub Saweris",
+            Email = "abanoub.saweris02@gmail.com",
+            Url = new Uri("https://github.com/AbanoubPhelopos/E-Commerce")
+        }
+    });
+});
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
