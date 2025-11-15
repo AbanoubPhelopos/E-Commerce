9f2d8d91f52180fcd29a0baad195c6c4ad2e827b
abanoub Saweris <abanoubphelopos12@gmail.com>
2025-11-15 16:47:59 +0200

    add basket entities

---
+++ services/basket/Basket.Core/Entities/ShoppingCart.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Basket.Core.Entities
+{
+    public class ShoppingCart
+    {
+        public string UserName { get; set; }
+        public List<ShoppingCartItems> Items { get; set; }=new();
+
+        public ShoppingCart()
+        {
+        }
+        public ShoppingCart(string userName)
+        {
+            UserName = userName;
+        }
+    }
+}
