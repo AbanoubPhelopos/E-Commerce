cc651ae297482724089d62aa6ae1e3669eef3e02
abanoub Saweris <abanoubphelopos12@gmail.com>
2025-11-20 14:48:19 +0200

    Create & Delete basket endpoints

--- services/basket/Basket.API/Controllers/BasketController.cs
+++ services/basket/Basket.API/Controllers/BasketController.cs
@@ -2,7 +2,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Basket.Application.Commands;
 using Basket.Application.Queries;
+using Basket.Application.Responses;
+using Basket.Core.Entities;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
@@ -17,11 +20,30 @@ namespace Basket.API.Controllers
         }
 
         [HttpGet("{action}/{username}")]
+        [ProducesResponseType(typeof(ShoppingCart), StatusCodes.Status200OK)]
         public async Task<IActionResult> GetBasket(string username)
         {
             var query = new GetBasketByUserNameQuery(username);
             var basket = await _mediator.Send(query);
             return Ok(basket);
         }
+
+        [HttpPost("{action}")]
+        [ProducesResponseType(typeof(ShoppingCart), StatusCodes.Status200OK)]
+        public async Task<ActionResult<ShoppingCartResponse>> CreateBasket([FromBody] CreateShoppingCartCommand command)
+        {
+            var result = await _mediator.Send(command);
+            return Ok(result);
+        }
+        
+        [HttpDelete("{action}/{username}")]
+        [ProducesResponseType(typeof(void), StatusCodes.Status200OK)]
+        public async Task<IActionResult> DeleteBasket(string username)
+        {
+            var command = new DeleteShoppinngCartCommand(username);
+            await _mediator.Send(command);
+            return Ok();
+        }
+
     }
 }
