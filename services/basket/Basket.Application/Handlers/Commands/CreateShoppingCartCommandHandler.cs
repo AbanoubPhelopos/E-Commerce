5b0d4e367b9e4abb4e9a52c5c23e7605f693ac42
abanoub Saweris <abanoubphelopos12@gmail.com>
2025-11-20 14:03:43 +0200

    implement create basket command handler

---
+++ services/basket/Basket.Application/Handlers/Commands/CreateShoppingCartCommandHandler.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Basket.Application.Commands;
+using Basket.Application.Responses;
+using Basket.Core.Entities;
+using Basket.Core.Repositories;
+using MediatR;
+
+namespace Basket.Application.Handlers.Commands
+{
+    public class CreateShoppingCartCommandHandler : IRequestHandler<CreateShoppingCartCommand, ShoppingCartResponse>
+    {
+        private readonly IBasketRepository _basketRepository;
+        private readonly IMapper _mapper;
+
+        public CreateShoppingCartCommandHandler(IBasketRepository basketRepository, IMapper mapper)
+        {
+            _basketRepository = basketRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<ShoppingCartResponse> Handle(CreateShoppingCartCommand request, CancellationToken cancellationToken)
+        {
+            var basket = new ShoppingCart
+            {
+                UserName = request.UserName,
+                Items = request.Items
+            };
+            var createdBasket = await _basketRepository.CreateBasketAsync(basket);
+            var response = _mapper.Map<ShoppingCartResponse>(createdBasket);
+            return response;
+        }
+    }
+}
