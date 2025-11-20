af214242b8c76c4105de268031d5a583b2f4da2b
abanoub Saweris <abanoubphelopos12@gmail.com>
2025-11-20 13:30:15 +0200

    create mapping profile for basket service

--- services/basket/Basket.Application/Mappers/BasketMappingProfile.cs
+++ services/basket/Basket.Application/Mappers/BasketMappingProfile.cs
@@ -2,11 +2,20 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
+using Basket.Application.Responses;
+using Basket.Core.Entities;
 
 namespace Basket.Application.Mappers
 {
-    public class BasketMappingProfile
+    public class BasketMappingProfile : Profile
     {
-        
+        public BasketMappingProfile()
+        {
+            CreateMap<ShoppingCart, ShoppingCartResponse>()
+                .ForMember(dest => dest.Items, opt => opt.MapFrom(src => src.Items)).ReverseMap();
+
+            CreateMap<ShoppingCartItems, ShoppingCartItemResponse>().ReverseMap();
+        }
     }
 }
