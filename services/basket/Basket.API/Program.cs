2fc574a37aeb18dc4c6b125ee01d32c5e124da0d
abanoub Saweris <abanoubphelopos12@gmail.com>
2025-11-20 14:30:08 +0200

    configure basket program.cs class

--- services/basket/Basket.API/Program.cs
+++ services/basket/Basket.API/Program.cs
@@ -1,10 +1,48 @@
+using System.Reflection;
+using Basket.Application.Commands;
+using Basket.Application.Mappers;
+using Basket.Core.Repositories;
+using Basket.Infrastructure.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
+builder.Services.AddAutoMapper(typeof(BasketMappingProfile).Assembly);
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(
+            Assembly.GetExecutingAssembly(),
+            Assembly.GetAssembly(typeof(CreateShoppingCartCommand))!));
+
+builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+
+builder.Services.AddApiVersioning(options =>
+{
+    options.AssumeDefaultVersionWhenUnspecified = true;
+    options.DefaultApiVersion = new ApiVersion(1, 0);
+    options.ReportApiVersions = true;
+});
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Title = "Catalog API",
+        Version = "v1",
+        Description = "this api for Catalog microservice in an e-commerce application",
+        Contact = new Microsoft.OpenApi.Models.OpenApiContact
+        {
+            Name = "Abanoub Saweris",
+            Email = "abanoub.saweris02@gmail.com",
+            Url = new Uri("https://github.com/AbanoubPhelopos/E-Commerce")
+        }
+    });
+});
+
 builder.Services.AddOpenApi();
+
 
 var app = builder.Build();
 
