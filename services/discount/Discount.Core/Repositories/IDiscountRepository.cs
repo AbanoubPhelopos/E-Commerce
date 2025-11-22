c04c3274d55d8de682e4999b2428395144ec9c2b
abanoub Saweris <abanoubphelopos12@gmail.com>
2025-11-22 20:47:22 +0200

    add core layer entities and repositories contract

---
+++ services/discount/Discount.Core/Repositories/IDiscountRepository.cs
@@ -0,0 +1,12 @@
+using Discount.Core.Entities;
+
+namespace Discount.Core.Repositories
+{
+    public interface IDiscountRepository
+    {
+        Task<Coupon?> GetDiscountAsync(string productName);
+        Task<bool> CreateDiscountAsync(Coupon coupon);
+        Task<bool> UpdateDiscountAsync(Coupon coupon);
+        Task<bool> DeleteDiscountAsync(string productName);
+    }
+}
