29459f6d2183e5f73c7cea132a64883bbd212df0
abanoub saweris <abanoubphelopos12@gmail.com>
2026-02-11 15:38:06 +0200

    create order context factory for design time inistance of orderContext

---
+++ services/ordering/Ordering.Infrastructure/Data/OrderContextFactory.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Ordering.Infrastructure.Data;
+
+public class OrderContextFactory : IDesignTimeDbContextFactory<OrderContext>
+{
+    public OrderContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<OrderContext>();
+        optionsBuilder.UseSqlServer("Server=localhost;Database=OrderingDb;User Id=sa;Password=P@ssw0rd123;TrustServerCertificate=True;");
+
+        return new OrderContext(optionsBuilder.Options);
+    }
+}
