d4b00198836cb3405d2a87e1f162e9bc0c3624f8
abanoub saweris <abanoubphelopos12@gmail.com>
2026-02-16 13:15:09 +0200

    implement Checkout Order Command and it's command

---
+++ services/ordering/Ordering.Application/Handlers/Commands/CheckoutOrderCommandHandler.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Ordering.Application.Commands;
+using Ordering.Core.Entities;
+using Ordering.Core.Repositories;
+
+namespace Ordering.Application.Handlers.Commands;
+
+public class CheckoutOrderCommandHandler(IOrderRepository orderRepository, IMapper mapper, ILogger<CheckoutOrderCommandHandler> logger) : IRequestHandler<CheckoutOrderCommand, int>
+{
+    private readonly IOrderRepository _orderRepository = orderRepository;
+    private readonly IMapper _mapper = mapper;
+    private readonly ILogger<CheckoutOrderCommandHandler> _logger = logger;
+    public async Task<int> Handle(CheckoutOrderCommand request, CancellationToken cancellationToken)
+    {
+        var order = _mapper.Map<Order>(request);
+        var generatedOrder = await _orderRepository.AddAsync(order, cancellationToken);
+        _logger.LogInformation($"Order {generatedOrder.Id} created");
+        return generatedOrder.Id;
+    }
+}
