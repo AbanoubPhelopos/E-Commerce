ed2037528d2d5b1341462ae978b536cd461ba6d3
abanoub saweris <abanoubphelopos12@gmail.com>
2026-02-16 13:45:03 +0200

    Update Order Command and it's Handler

---
+++ services/ordering/Ordering.Application/Handlers/Commands/UpdateOrderCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Ordering.Application.Commands;
+using Ordering.Application.Exceptions;
+using Ordering.Core.Entities;
+using Ordering.Core.Repositories;
+
+namespace Ordering.Application.Handlers.Commands;
+
+public class UpdateOrderCommandHandler (IOrderRepository orderRepository, IMapper mapper, ILogger<CheckoutOrderCommandHandler> logger):IRequestHandler<UpdateOrderCommand,Unit>
+{
+    private readonly IOrderRepository _orderRepository = orderRepository;
+    private readonly IMapper _mapper = mapper;
+    private readonly ILogger<CheckoutOrderCommandHandler> _logger = logger;
+    
+    public async Task<Unit> Handle(UpdateOrderCommand request, CancellationToken cancellationToken)
+    {
+        var orderToUpdate = await _orderRepository.GetByIdAsync(request.Id,cancellationToken);
+        if (orderToUpdate == null)
+        {
+            throw new OrderNotFoundException(nameof(Order),$"Order with id {request.Id} not found");
+        }
+        await _orderRepository.UpdateAsync(orderToUpdate, cancellationToken);
+        _logger.LogInformation($"Order {request.Id} updated");
+        return Unit.Value;
+    }
+}
