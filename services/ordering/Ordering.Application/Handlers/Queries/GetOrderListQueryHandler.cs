d62a122c254390e7a6e046eae35118d8ee6b4b37
abanoub saweris <abanoubphelopos12@gmail.com>
2026-02-16 13:09:06 +0200

    implement the Get Order List Query and it's handler

---
+++ services/ordering/Ordering.Application/Handlers/Queries/GetOrderListQueryHandler.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using MediatR;
+using Ordering.Application.Queries;
+using Ordering.Application.Responses;
+using Ordering.Core.Repositories;
+
+namespace Ordering.Application.Handlers.Queries;
+
+public class GetOrderListQueryHandler(IOrderRepository orderRepository, IMapper mapper) : IRequestHandler<GetOrderListQuery, List<OrderResponse>>
+{
+    private readonly IOrderRepository _orderRepository = orderRepository;
+    private readonly IMapper _mapper = mapper;
+    
+    public async Task<List<OrderResponse>> Handle(GetOrderListQuery request, CancellationToken cancellationToken)
+    {
+        var orders = await _orderRepository.GetOrdersByCustomerNameAsync(request.UserName, cancellationToken);
+        var orderResponses = _mapper.Map<List<OrderResponse>>(orders);
+        return orderResponses;
+    }
+}
